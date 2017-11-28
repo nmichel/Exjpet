59d247e299a0d099cb93867ce96abb5a8439b974
Nicolas Michel <nicolas.michel.lava@gmail.com>
2017-11-28 15:27:45 +0000

    Rework Exjpet.Delegate (+ doc)

--- lib/exjpet/delegate.ex
+++ lib/exjpet/delegate.ex
@@ -1,22 +1,70 @@
 defmodule Exjpet.Delegate do
   @moduledoc """
-  TODO
+  `Exjpet.Delegate` inject functions delegation to the target module, for all
+  exported functions of that module.
+
+  ## Example
+
+  The following module proxies Erlang `:maps` module.
+  For illustration purpose it also exports a `foo` function.
+
+      iex> defmodule MyMap do
+      ...>   use Exjpet.Delegate, :maps
+      ...>   def foo do
+      ...>     :foo
+      ...>   end
+      ...> end
+      iex> MyMap.get(:a, %{a: 42})
+      42
+      iex> MyMap.to_list(%{a: 42, b: "foo"})
+      [a: 42, b: "foo"]
+      iex> MyMap.foo
+      :foo
+
+  ## Aliasing
+
+  Using alias as target module is not supported.
+
+  Following declaration will compile
+
+      defmodule DelegateToList do
+        use Exjpet.Delegate, List # ok
+      end
+
+  But that one won't
+
+      defmodule FailedDelegateToList do
+        alias List, as: MyList
+        use Exjpet.Delegate, MyList # failed !
+      end
   """
 
   defmacro __using__(module) do
-    specs =
-      module.module_info[:exports]
-      |> Kernel.--([{:module_info, 0}, {:module_info, 1}, {:__info__, 1}])
-
     asts =
-      specs |> Enum.map(&build_signature(&1, __CALLER__.module, module))
+      module
+      |> resolve()
+      |> apply(:module_info, [])
+      |> Keyword.fetch!(:exports)
+      |> Kernel.--([{:module_info, 0}, {:module_info, 1}, {:__info__, 1}])
+      |> Enum.map(&build_signature(&1, __CALLER__.module, module))
 
     quote do
       unquote_splicing(asts)
     end
   end
 
-  def build_signature({name, count}, mod, target) do
+  defp resolve({:__aliases__, _ctxt, names}) do
+    names
+    |> Enum.join(".")
+    |> (&("Elixir."<>&1)).()
+    |> to_string()
+    |> String.to_atom()
+  end
+  defp resolve(module) when is_atom(module) do
+    module
+  end
+
+  defp build_signature({name, count}, mod, target) do
     quote do
       defdelegate unquote(name)(unquote_splicing(Macro.generate_arguments(count, mod))), to: unquote(target)
     end
--- test/exjpet/delegate_test.exs
+++ test/exjpet/delegate_test.exs
@@ -2,22 +2,36 @@ defmodule Exjpet.DelegateTest.Source do
   def echo(what) do
     Atom.to_string(__MODULE__) <> " " <> what
   end
-
-  def bar do
-    Atom.to_string(__MODULE__) <> " bar"
-  end
 end
 
 defmodule Exjpet.DelegateTest.Target  do
-  use Exjpet.Delegate, :'Elixir.Exjpet.DelegateTest.Source'
+  use Exjpet.Delegate, :"Elixir.Exjpet.DelegateTest.Source"
+end
+
+defmodule Exjpet.DelegateTest.Target2  do
+  use Exjpet.Delegate, Exjpet.DelegateTest.Source
+
+  def foo() do
+    :foo
+  end
 end
 
 defmodule Exjpet.DelegateTest do
   use ExUnit.Case
   doctest Exjpet.Delegate
   
-  test "Target delegates to Source" do
+  test "delegates to Elixir module (atom)" do
     alias Exjpet.DelegateTest.Target
     assert Target.echo("foo") == "Elixir.Exjpet.DelegateTest.Source foo"
+  end
+
+  test "delegates to Elixir module (Elixir module)" do
+    alias Exjpet.DelegateTest.Target2, as: Target
+    assert Target.echo("bar") == "Elixir.Exjpet.DelegateTest.Source bar"
+  end
+
+  test "delegate module can contain own function" do
+    alias Exjpet.DelegateTest.Target2, as: Target
+    assert Target.foo() == :foo
   end
 end
