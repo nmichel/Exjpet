32860360c743688c4f43b1a35a4951c784056a39
Nicolas Michel <nicolas.michel.lava@gmail.com>
2017-12-05 10:06:55 +0000

    Do not reinvent the wheel of dependency management

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule Exjpet.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps(Mix.env)
+      deps: deps()
     ]
   end
 
@@ -18,22 +18,12 @@ defmodule Exjpet.Mixfile do
     ]
   end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps(:test) do
-    mandatory_deps() ++
-    [
-      {:httpoison, "~> 0.13.0"},
-    ]
-  end
-
-  defp deps(_) do
-    mandatory_deps()
-  end
-
-  defp mandatory_deps do
+  defp deps do
     [
       {:poison, "~> 3.1"},
-      {:ejpet, "~> 0.7.0"}
+      {:ejpet, "~> 0.7.0"},
+      {:ex_doc, "~> 0.16", only: :dev, runtime: false},
+      {:httpoison, "~> 0.13.0", only: :test, runtime: false}
     ]
   end
 end
