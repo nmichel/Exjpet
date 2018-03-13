47c97f8d840bf0b0064a633a91564fe438d57556
Nicolas Michel <nicolas.michel.lava@gmail.com>
2018-03-13 19:46:35 +0000

    Add package metadata

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule Exjpet.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -14,6 +16,19 @@ defmodule Exjpet.Mixfile do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  defp description() do
+    "The easy way to ejpet for Elixir"
+  end
+
+  defp package() do
+    [
+      name: "exjpet",
+      maintainers: ["Nicolas Michel"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/nmichel/Exjpet"}
     ]
   end
 
